26cfab12fba781d8c7378766dfa6e17b9f7b3af4
Rek Magar <rekmagar@gmail.com>
2022-10-25 07:06:41 +0545

    Repository Created for 1st Time

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("Hello");
+var person = {
+    firstName:"John",
+    lastName:"Doe",
+    age:20,
+    nationality:"German"
+    };
+console.log(typeof(person));
+
+var a = null;
+console.log(a);
